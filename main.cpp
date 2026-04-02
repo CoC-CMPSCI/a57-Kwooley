3061cee61c8fd6ee3bc7ed901e5d5cd2c2852d14
kyu.lee_admin <kyu.lee@avc.edu>
2026-04-01 18:54:04 -0700

    A57

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@ using namespace std;
 int main()
 {
 	int n1, n2;
-	int N, sum;
+	int N, next;
 
+	cout << "Enter two numbers: ";
 	cin >> n1 >> n2;
+	cout << "Enter the number of sequences: ";
 	cin >> N;
+	cout << n1 << "\t" << n2 << "\t";
 
-	// TODO
-	// TODO
+	for (int i =0; i < N-2; i++){
+		next = n1 + n2;
+		cout << next << "\t";
+		n1 = n2;
+		n2 = next;
+	}
+	cout <<endl;
 }
